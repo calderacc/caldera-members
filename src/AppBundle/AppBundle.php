f59a6ffb6d02e31490c01bb282a9d305832133e3
Malte Hübner <maltehuebner@gmx.org>
2016-12-16 01:16:52 +0100

    Rename AppBundle to CalderaMembersBundle.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Caldera\Bundle\MembersBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class CalderaMembersBundle extends Bundle
 {
 }
