c19020ba729e0ad9c0245f2b2589451ab9c898d7
Malte Hübner <maltehuebner@gmx.org>
2016-12-17 17:13:00 +0100

    Okay, we will get that done later.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,8 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
             new Caldera\Bundle\MembersBundle\CalderaMembersBundle()
         ];
 
