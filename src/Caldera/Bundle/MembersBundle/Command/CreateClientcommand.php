f3f1f8d4fd1b9d16e14e29f8d3fc51258f74c296
Malte Hübner <maltehuebner@gmx.org>
2016-12-17 13:30:08 +0100

    Add configure() block.

--- src/Caldera/Bundle/MembersBundle/Command/CreateClientcommand.php
+++ src/Caldera/Bundle/MembersBundle/Command/CreateClientcommand.php
@@ -3,8 +3,29 @@
 namespace Caldera\Bundle\MembersBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 
 class CreateClientcommand extends ContainerAwareCommand
 {
-
+    protected function configure()
+    {
+        $this
+            ->setName('fos:oauth-server:create-client')
+            ->setDescription('Create an new OAuth client')
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'Client name'
+            )
+            ->addArgument(
+                'redirect-uri',
+                InputArgument::REQUIRED,
+                'Select an uri to redirect users'
+            )
+            ->addArgument(
+                'grant-type',
+                InputArgument::REQUIRED,
+                'List of grantable types'
+            );
+    }
 }
